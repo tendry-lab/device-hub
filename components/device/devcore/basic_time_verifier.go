1b0a5b9aac97ce6f65f5618efa4b1e8ef4365500
Dmitriy Shilin <dshil@fastmail.com>
2025-10-24 13:33:51 +0200

    Update license

--- components/device/devcore/basic_time_verifier.go
+++ components/device/devcore/basic_time_verifier.go
@@ -1,3 +1,8 @@
+/*
+ * SPDX-FileCopyrightText: 2025 Tendry Lab
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 package devcore
 
 // BasicTimeVerifier ensures that the device UNIX time is greater than 0.
--- components/system/syssched/task_alive_notifier.go
+++ components/system/syssched/task_alive_notifier.go
@@ -1,3 +1,8 @@
+/*
+ * SPDX-FileCopyrightText: 2025 Tendry Lab
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 package syssched
 
 // TaskAliveNotifier to notify if the task is running without errors.
